be394628f839d9556183000262183b3de6479e28
Your Name <aregmuradyan.dev@gmail.com>
2026-08-07 20:04:28 +0400

    Initialize Spring Boot project

---
+++ src/main/java/com/polixis/companysearch/CompanySearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polixis.companysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanySearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanySearchServiceApplication.class, args);
+	}
+
+}
